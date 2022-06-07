57684bfc6d02fa3db692dc2b057ba73b51fd563a
bangnokia <bangnokia@gmail.com>
2022-06-07 16:12:18 +0700

    work in progress

---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BangNokia\DauUi;
+
+use Illuminate\Support\Facades\Blade;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        Blade::anonymousComponentNamespace(__DIR__.'./resources/components', 'd');
+    }
+}
