bbec4413a8deafea6968b9b4a73622ab285b4595
bangnokia <bangnokia@gmail.com>
2022-06-08 20:11:33 +0700

    update form group

--- resources/views/components/form-group.blade.php
+++ resources/views/components/form-group.blade.php
@@ -1,9 +1,15 @@
 @props(['label', 'error' => null])
 
-<div {{ $attributes }} x-data x-id="['input']">
-    <label x-bind:for="$id('input')" for="first-name" class="block text-sm font-medium text-gray-700">{{ $label }}</label>
+<div {{ $attributes }}
+     x-data
+     @if ($label) x-id="['input']" @endif
+>
+    @if ($label)
+        <label x-bind:for="$id('input')"
+               for="first-name"
+               class="block text-sm font-medium text-gray-700">{{ $label }}</label>
+    @endif
     {{ $slot }}
-
     @if ($error)
         <div class="mt-1 text-rose-500 text-sm">{{ $error }}</div>
     @endif
