ba9d9862af310ae325fe03477dc8dcc4a551d288
bangnokia <bangnokia@gmail.com>
2022-06-08 19:43:19 +0700

    add file input

--- resources/views/components/file.blade.php
+++ resources/views/components/file.blade.php
@@ -1,19 +1,14 @@
 @aware(['error'])
 
 @props(['readonly' => false])
-<label
-    tabindex="0"
-    x-bind:for="$id('input')"
+
+<input
+    x-bind:id="$id('input')"
     {{ $attributes->class([
-        'mt-1 border border-gray-300 px-2 py-1.5 outline outline-1 outline-transparent focus:outline-main-500 focus:border-main-500 block w-full shadow-sm sm:text-sm rounded-md',
+        'mt-1 border border-gray-300 px-3 py-2 outline outline-1 outline-transparent focus:outline-main-500 focus:border-main-500 block w-full shadow-sm sm:text-sm rounded-md',
         'border-rose-500' => $error,
         'border-gray-300' => !$error
-    ]) }}>
-    <input
-        x-bind:id="$id('input')"
-        class="w-full focus:outline-none"
-        type="file"
-        tabindex="-1"
-        @if ($readonly) readonly @endif
-    >
-</label>
+    ]) }}
+    type="file"
+    @if ($readonly) readonly @endif
+>
