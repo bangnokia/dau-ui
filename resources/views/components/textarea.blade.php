671d17c3638eab11d47248dd9c039d0817ba3378
bangnokia <bangnokia@gmail.com>
2022-06-08 19:27:04 +0700

    work in progress

---
+++ resources/views/components/textarea.blade.php
@@ -0,0 +1,12 @@
+@aware(['error'])
+
+@props(['readonly' => false])
+<textarea
+    x-bind:id="$id('input')"
+    {{ $attributes->class([
+        'mt-1 focus:ring-main-500 focus:border-main-500 block w-full shadow-sm sm:text-sm rounded-md',
+        'border-rose-500' => $error,
+        'border-gray-300' => !$error
+    ])->merge(['rows' => 3]) }}
+    @if ($readonly) readonly @endif
+>{{ $slot }}</textarea>
