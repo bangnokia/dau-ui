648b9db7e84e1811b2c2aceb743d8944ce685db6
bangnokia <bangnokia@gmail.com>
2022-06-09 15:03:01 +0700

    add checkbox

---
+++ resources/views/components/radio.blade.php
@@ -0,0 +1,12 @@
+@props(['label' => null])
+
+<div class="inline-flex items-center"
+     @if ($label) x-data x-id="['radio']" @endif>
+    <input type="radio"
+           x-bind:id="$id('radio')"
+        {{ $attributes->class('focus:ring-main-500 h-4 w-4 text-main-600 border-gray-300') }} />
+    @if ($label)
+        <label class="ml-2 text-sm text-gray-600" x-bind:for="$id('radio')">{{ $label }}</label>
+    @endif
+</div>
+
---
+++ resources/views/example.blade.php
@@ -0,0 +1,45 @@
+<div class="mx-auto bg-white flex items-center justify-center">
+    <form class="w-[500px] p-6 flex flex-col gap-6">
+        <x-d::form-group label="Name" error="Your name is soooooooo weak">
+            <x-d::input/>
+        </x-d::form-group>
+
+        <x-d::form-group label="Email">
+            <x-d::input type="email"/>
+        </x-d::form-group>
+
+        <x-d::form-group label="Description">
+            <x-d::textarea/>
+        </x-d::form-group>
+
+        <x-d::form-group label="Your resume">
+            <x-d::file/>
+        </x-d::form-group>
+
+        <x-d::form-group label="Your role">
+            <x-d::select
+                :options="['Admin', 'User', 'Senior', 'Pro']"
+                placeholder="Please select your role"
+            />
+        </x-d::form-group>
+
+        <x-d::form-group label="Checkboxes">
+            <div class="flex gap-4">
+                <x-d::checkbox label="PHP"/>
+                <x-d::checkbox label="Javascript"/>
+                <x-d::checkbox label="Rust"/>
+                <x-d::checkbox label="Svelte"/>
+            </div>
+        </x-d::form-group>
+
+        <x-d::form-group label="Radio">
+            <div class="flex gap-4">
+                <x-d::radio label="PHP" name="program-language"/>
+                <x-d::radio label="Swift" name="program-language"/>
+                <x-d::radio label="Rust" name="program-language"/>
+                <x-d::radio label="C++" name="program-language"/>
+            </div>
+        </x-d::form-group>
+
+    </form>
+</div>
