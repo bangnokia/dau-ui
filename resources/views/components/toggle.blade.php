af3528bf59cf43672a2a03e1a7979788b484def7
bangnokia <bangnokia@gmail.com>
2022-06-10 19:50:01 +0700

    change disable color of toggle when off to white

--- resources/views/components/toggle.blade.php
+++ resources/views/components/toggle.blade.php
@@ -7,6 +7,6 @@
 >
     <input type="checkbox" class="hidden peer" x-model="checked">
     <span
-        class="w-5 h-5 rounded-full transition bg-gray-400 translate-x-0 peer-checked:bg-white peer-checked:translate-x-5"
+        class="w-5 h-5 rounded-full transition bg-white translate-x-0 peer-checked:translate-x-5"
         aria-hidden="true"></span>
 </label>
