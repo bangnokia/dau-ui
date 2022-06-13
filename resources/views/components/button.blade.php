1b5ac9fdd2bdd572fefe3938ca0804ac36f00d30
bangnokia <bangnokia@gmail.com>
2022-06-14 00:24:41 +0700

    add focus to button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,6 +1,6 @@
 @props(['type' => 'button'])
 <button type="{{ $type }}" {{
-    $attributes->class('rounded px-4 py-2 text-sm border border-gray-300 rounded hover:bg-gray-100')
+    $attributes->class('rounded px-4 py-2 text-sm border border-gray-300 rounded hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-main-500')
 }}>
     {{ $slot }}
 </button>
--- resources/views/components/main-button.blade.php
+++ resources/views/components/main-button.blade.php
@@ -1,6 +1,6 @@
 @props(['type' => 'button'])
 <button type="{{ $type }}"
-       {{  $attributes->class("px-4 py-2 bg-main-500 rounded text-sm text-white border border-main-500 hover:bg-main-600") }}
+       {{  $attributes->class("px-4 py-2 bg-main-500 rounded text-sm text-white border border-main-500 hover:bg-main-600 focus:outline-none focus:ring-2 focus:ring-main-500") }}
 >
     {{ $slot }}
 </button>
